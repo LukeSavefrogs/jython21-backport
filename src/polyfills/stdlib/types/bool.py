b805532781e1e4f6cc4d1273840346b46fb7c0b0
Luca Salvarani <lucasalvarani99@gmail.com>
2023-05-15 15:18:28 +0200

    refactor(stdlib): Fix intellisense for `bool` value type

--- src/polyfills/stdlib/types/bool.py
+++ src/polyfills/stdlib/types/bool.py
@@ -4,11 +4,11 @@ class bool(int):
     """ Backport of the boolean values (`True` and `False`), which were introduced in Python 2.3. """
 
     def __init__(self, value):
+        # type: (int | str | list | dict | tuple) -> None
         """ Returns True when the argument x is true, False otherwise. The
         builtins True and False are the only two instances of the class bool.
         The class bool is a subclass of the class int, and cannot be subclassed.
         """
-        # type: (int | str | list | dict | tuple) -> None
         if type(value) == type(5) or type(value) == type(1 == 1):
             self.value = int(value) != 0
         elif type(value) in [type(""), type(u""), type([]), type(()), type({})]:
