149fc9c22a9fc6e76a676f6ff36f535136ca7381
Luca Salvarani <lucasalvarani99@gmail.com>
2023-05-23 16:39:19 +0200

    ci(tests): Add basic CLI interface to `run_tests.py`

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,3 +1,4 @@
+import getopt
 import os
 import re
 import sys
@@ -87,7 +88,7 @@ def detect_package_folder(current_directory=os.getcwd()):
 
     Args:
         current_directory (str): The current working directory.
-    
+
     Returns:
         str: The path to the package root.
     """
@@ -133,7 +134,7 @@ def detect_package_folder(current_directory=os.getcwd()):
         return current_directory
 
 
-if __name__ == "__main__":
+def discovery(verbosity=1):
     package_path = detect_package_folder("./src/polyfills")
     print("Project folder is '%s'" % package_path)
 
@@ -142,12 +143,39 @@ if __name__ == "__main__":
     print("Found %d test files:" % len(test_files))
     print("\n".join(["- " + file for file in test_files]) + "\n")
 
-
     # Add the package path to the PYTHONPATH so that tests can import the package
     sys.path.append(os.path.dirname(package_path))
 
     # Run the tests
     suite = unittest.TestLoader().loadTestsFromNames(test_files)
-    test_result = unittest.TextTestRunner(verbosity=2).run(suite)
+    test_result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
 
-    sys.exit(not test_result.wasSuccessful())
+    return test_result.wasSuccessful()
+
+
+# Command line interface
+if __name__ == "__main__":
+    tests_verbosity = 1
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hvq",
+            ["help", "verbose", "quiet"],
+        )
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print("Usage: run_tests.py [-h] [-v] [-q]")
+            sys.exit()
+
+        elif opt in ("-v", "--verbose"):
+            tests_verbosity = 2
+
+        elif opt in ("-q", "--quiet"):
+            tests_verbosity = 0
+
+    sys.exit(not discovery(verbosity=tests_verbosity))
